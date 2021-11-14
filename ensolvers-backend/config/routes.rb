37d0df458da8d99b002390da9a9cfc13780f0164
Kevin <kevinsevinche@gmail.com>
2021-11-14 02:09:38 -0500

    update routes

---
+++ ensolvers-backend/config/routes.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :users
+      resources :folders do
+        resources :tasks
+      end
+      resources :todos
+
+      post :login, to: 'auth#create'
+    end
+  end
+end
